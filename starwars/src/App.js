fcf37dce44eed346af869892d043a59c6ce13460
Allan <allanoliveira.dev@gmail.com>
2020-06-28 20:48:31 -0300

    completed basic Header component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from "./components/HeaderContainer/HeaderSection"
 import './App.css';
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Header/>
     </div>
   );
 }
---
+++ starwars/src/components/HeaderContainer/HeaderSection.js
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "styled-components"
+import { FaHeart } from "react-icons/fa"
+import "../../App.css"
+
+export const UserSection = styled.h3`
+    color: red;
+    margin-left: 7rem;
+`
+
+export const SearchSection = styled.input`
+    background-color: white;
+    margin-left: 15rem
+`
+
+export const FavoriteCharacters = styled.div`
+    width: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const Header = () => {
+    return (
+        <div className={"header-container"}>
+            <UserSection>Allan Oliveira</UserSection>
+            <SearchSection placeholder="search characters"/>
+            <FavoriteCharacters>
+                <p>Favorites</p>
+                <FaHeart/>
+            </FavoriteCharacters>
+        </div>
+    )
+}
+
+export default Header
